69e4e1fe7fcc25faf9d2dc0a34763925c3d1d241
Devang Bramhakshatriya <devangbramhakshtriya@gmail.com>
2022-12-16 03:52:41 +0530

    File Added

--- yoox/src/App.js
+++ yoox/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <Home/>
      <Footer/>
     </div>
   );
---
+++ yoox/src/Pages/Home.jsx
@@ -0,0 +1,42 @@
+import { Grid, GridItem,Image } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import ProductCart from "../Components/ProductCart";
+
+function Home(){
+    const [data,setData]=useState([]);
+    useEffect(()=>{
+        fetch(`http://localhost:8080/data`)
+        .then((res)=>res.json())
+        .then((res)=>setData(res))
+        .catch((err)=>console.log(err))
+    },[])
+    console.log(data)
+    return(
+        <>
+        <Image w="100%" src="https://www.yoox.com/images/yoox80/banners/6895_1_Genz_WM_Main.png?634485886869569819&impolicy=cropDefault&width=960&height=510"/>
+        <Grid templateColumns="repeat(4,1fr)" gap={6}>
+            {
+                data?.length>0&&data.map((e)=>{
+                    return(
+                        <GridItem key={e.Image}>
+                            <ProductCart
+                            Image={e.Image}
+                            category={e.category}
+                            NewPrice={e.NewPrice}
+                            OldPrice={e.OldPrice}
+                            Offer={e.Offer}
+                            brand={e.brand}
+                            aSize={e.aSize? e.aSize:""}
+                            aSize2={e.aSize2? e.aSize2:""}
+                            aSize3={e.aSize3? e.aSize3:""}
+                            aSize4={e.aSize4? e.aSize4:""}
+                            />
+                        </GridItem>
+                    )
+                })
+            }
+        </Grid>
+        </>
+    )
+}
+export default Home;
